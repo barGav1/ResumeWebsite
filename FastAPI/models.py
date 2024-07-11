d393cef77bb4eb69b9a3a09b6e4dc42cc0233f1e
barGav1 <82818739+barGav1@users.noreply.github.com>
2024-07-11 18:06:14 +0300

    Delete FastAPI directory

--- FastAPI/models.py
+++
@@ -1,9 +0,0 @@
-from database import Base
-from sqlalchemy import Column , String , Integer
-
-class Users(Base):
-    __tablename__ = "users"
-
-    id = Column(Integer, primary_key=True, index=True)
-    email = Column(String)
-    password = Column(String)
